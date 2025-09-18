39e4bd453bf3129fad1079722fe3e4daa14a7694
fatimazahrae <assilafatimazahra440@gmail.com>
2025-09-18 10:42:40 +0100

    chapitres js

---
+++ Chap3/script2.js
@@ -0,0 +1,11 @@
+let age = 27;
+if ( age < 18){
+    console.log( "your are minor");
+}
+else if ( age === 18){
+    console.log(" you have just reached the majority");
+}
+else{
+    console.log("you are major");
+}
+
--- scrip.js
+++
@@ -1,4 +0,0 @@
-let num1 = 10;
-let num2 = 5;
-let sum = num1 + num2;
-console.log("La somme est : " + sum);
