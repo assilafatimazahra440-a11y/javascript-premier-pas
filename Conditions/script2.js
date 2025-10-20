35fcaeab14d17f4a8866d1d96b9d407ab5633789
fatima <assilafatimazahra440@gmail.com>
2025-10-20 11:39:10 +0100

    commit

--- Conditions/script2.js
+++ Conditions/script2.js
@@ -1,4 +1,4 @@
-let age = 27;
+let age = 18;
 if ( age < 18){
     console.log( "your are minor");
 }
--- lesBoucles/boucl.js
+++ lesBoucles/boucl.js
@@ -1,7 +1,14 @@
-for (let i = 1;
+/*for (let i = 1;
     i <= 10; 
     i++)
     {
     if ( i === 5) continue;
     console.log("number" + i);
-    }
+    }*/
+    let table = ["C4", "C4", "C2", "C3", "C8", "C4", "C3"];
+let take = "C7";
+let first = 0;
+let i = 0;
+while(i < table.length)
+
+console.log("the code", take, "appears", first, "times");
